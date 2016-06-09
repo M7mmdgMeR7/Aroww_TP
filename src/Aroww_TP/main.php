a2460dd5333ed469c5644786c62b952cbcead1aa
M7mmdgMeR7 <Mohalaamer@icloud.com>
2016-06-09 11:26:39 +0300

    Create main.php

---
+++ src/Aroww_TP/main.php
@@ -0,0 +1,43 @@
+<?php
+
+use Pocketmine\Plugin\PluginBase
+
+Class main extends PluginBase{
+
+public function onEnable(){
+
+   $this->getServer()->getLogger()->info("plugin aroww_tp has enable");
+ 
+       public function OnDisable(){
+
+    $this->getServer()->getLogger()->info("plugin aroww_tp has Disable");
+
+
+ 
+   public function onShoot (\pocketmine\event\entity\EntityShootBowEvent $event)
+		{
+				$shooter = $event->getEntity();
+				if ($shooter instanceof Player) {
+					$id = $shooter->getID();
+					$this->launch[$id] = (bool) true;
+				}
+		}
+		
+		function onHit (\pocketmine\event\entity\ProjectileHitEvent $event)
+		{
+				$entity = $event->getEntity();
+				$level = $entity->getLevel();
+				if ($entity instanceof Arrow) {
+					$shooter = $entity->shootingEntity;
+					$pos = $entity->getPosition();
+					if ($shooter instanceof Player) {
+						$id = $shooter->getID();
+						$shoot = $this->launch[$id];
+						if ($pos instanceof Position and isset($shoot)) {
+							$level->removeEntity($entity);
+							$event->teleport($pos);
+							unset($this->launch[$id]);
+						}
+					}
+				}
+		}     
